f21904cf066e13f28b82aaf7df4f98added2856e
vinhutl <72056963+vinhutl@users.noreply.github.com>
2025-10-20 18:32:35 -0700

    remove growden

--- growden.io/s/pwa.js
+++
@@ -1,8 +0,0 @@
-window.deferredInstall = null;
-window.addEventListener('beforeinstallprompt', (e) => {
-  window.deferredInstall = e;
-  console.log('PWA install prompt has been saved.');
-  if (typeof unityInstance !== 'undefined' && unityInstance != null) {
-    unityInstance.SendMessage('PWAManager', 'OnPwaPromptAvailabilityChanged', window.deferredInstall ? "1" : "0");
-  }
-});
